e37c975a472ef1d275390de82a8d5413b00dd0df
Emmanuel Okum <emmanuel.okum@idea2solutions.com>
2026-06-27 23:22:13 +0100

    remove flux from the project

--- resources/views/flux/navlist/group.blade.php
+++ resources/views/flux/navlist/group.blade.php
@@ -6,46 +6,33 @@
 
 <?php if ($expandable && $heading): ?>
 
-<ui-disclosure
-    {{ $attributes->class('group/disclosure') }}
-    @if ($expanded === true) open @endif
-    data-flux-navlist-group
->
-    <button
-        type="button"
-        class="group/disclosure-button mb-[2px] flex h-10 w-full items-center rounded-lg text-zinc-500 hover:bg-zinc-800/5 hover:text-zinc-800 lg:h-8 dark:text-white/80 dark:hover:bg-white/[7%] dark:hover:text-white"
-    >
-        <div class="ps-3 pe-4">
-            <flux:icon.chevron-down class="hidden size-3! group-data-open/disclosure-button:block" />
-            <flux:icon.chevron-right class="block size-3! group-data-open/disclosure-button:hidden" />
+    <div {{ $attributes->class('group mb-1') }}>
+        <button type="button" class="flex items-center w-full rounded-lg text-zinc-500 hover:bg-zinc-50 hover:text-zinc-800 p-2">
+            <svg class="h-4 w-4 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"/></svg>
+            <span class="text-sm font-medium leading-none">{{ $heading }}</span>
+        </button>
+
+        <div class="mt-2 ps-4">
+            {{ $slot }}
         </div>
-
-        <span class="text-sm font-medium leading-none">{{ $heading }}</span>
-    </button>
-
-    <div class="relative hidden space-y-[2px] ps-7 data-open:block" @if ($expanded === true) data-open @endif>
-        <div class="absolute inset-y-[3px] start-0 ms-4 w-px bg-zinc-200 dark:bg-white/30"></div>
-
-        {{ $slot }}
     </div>
-</ui-disclosure>
 
 <?php elseif ($heading): ?>
 
-<div {{ $attributes->class('block space-y-[2px]') }}>
-    <div class="px-1 py-2">
-        <div class="text-xs leading-none text-zinc-400">{{ $heading }}</div>
-    </div>
+    <div {{ $attributes->class('block space-y-2') }}>
+        <div class="px-1 py-2">
+            <div class="text-xs leading-none text-zinc-400">{{ $heading }}</div>
+        </div>
 
-    <div>
-        {{ $slot }}
+        <div>
+            {{ $slot }}
+        </div>
     </div>
-</div>
 
 <?php else: ?>
 
-<div {{ $attributes->class('block space-y-[2px]') }}>
-    {{ $slot }}
-</div>
+    <div {{ $attributes->class('block space-y-2') }}>
+        {{ $slot }}
+    </div>
 
 <?php endif; ?>
